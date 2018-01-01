32325cd7a3a015a0046cea05c1873bf96233ce71
Reza Zeiny <rezazeiny1998@gmail.com>
2018-01-01 15:05:56 +0330

    end of project

--- LED.py
+++ LED.py
@@ -1,31 +1,53 @@
-# from RPi.GPIO import GPIO
+from RPi.GPIO import GPIO
 import time
 
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 
 class LED:
-    def __init__(self, gpio_number):
-        self.gpio_number = gpio_number
-        # GPIO.setup(gpio_number, GPIO.OUT)
+    def __init__(self):
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.setup(9, GPIO.OUT)
+        GPIO.setup(10, GPIO.OUT)
+        GPIO.setup(25, GPIO.OUT)
+        GPIO.setup(8, GPIO.OUT)
 
-    def turn_on(self):
-        print("on")
-        # GPIO.output(self.gpio_number, GPIO.HIGH)
+    def change_num(self, num):
+        num = num - 1
+        num = num % 6 + 1
+        if num == 1:
+            return 7
+        if num == 2:
+            return 11
+        if num == 3:
+            return 9
+        if num == 4:
+            return 10
+        if num == 5:
+            return 25
+        if num == 6:
+            return 8
 
-    def turn_off(self):
-        print("Off")
-        # GPIO.output(self.gpio_number, GPIO.LOW)
+    def turn_on(self, num):
+        GPIO.output(self.change_num(num), GPIO.HIGH)
+
+    def turn_off(self, num):
+        GPIO.output(self.change_num(num), GPIO.LOW)
 
 
-led_list = [LED(7), LED(11)]
-# led_list.append(LED(7))
-# led_list.append(LED(11))
-# led_list.append(LED(9))
-# led_list.append(LED(10))
-# led_list.append(LED(25))
-# led_list.append(LED(8))
-
-led_list[0].turn_on
+led = LED()
+while True:
+    for j in range(1, 5):
+        for i in range(j, j + 6):
+            led.turn_on(i)
+            time.sleep(0.5)
+            led.turn_off(i - j + 1)
+        led.turn_on(j)
+        time.sleep(0.5)
+    led.turn_on(6)
+    time.sleep(0.5)
+    for i in range(1, 7):
+        led.turn_off(i)
